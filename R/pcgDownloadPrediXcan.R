2cb994d15038fd36804193d8c3d270075ec3703a
Darius Bost <dariusbost@dhcp152-54-6-191.wireless.europa.renci.org>
2019-08-22 14:23:22 -0400

    First commit -- setup package structure
    Setup the package structure, started work on
    pcgDownloadPrediXcan.

---
+++ R/pcgDownloadPrediXcan.R
@@ -0,0 +1,10 @@
+#' @name pgcDownloadPrediXcan
+#' @title Download the PrediXcan database
+#' @param destDir character of length 1, the download directory
+#' @export
+
+pgcDownloadPrediXcan <- function(destDir = "PrediXcan") {
+  dir.exists(destDir)
+  dir.create(destDir)
+  download.file(, destfile = destDir)
+}
