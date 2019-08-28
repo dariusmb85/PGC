43f62ab2651557ae1c6e7740fc804a07b8169079
Dayne <dayne.filer@gmail.com>
2019-08-28 15:02:16 -0400

    Edits from Dayne

--- R/pgcCombinePredixFiles.R
+++ R/pgcCombinePredixFiles.R
@@ -1,28 +1,33 @@
 #' @name pgcCombinePredxFiles
 #' @title Combine Predixcan database files
 #' @param destDir character of length 1, the download directory
-#' @import RSQLite
+#' @details
+#' \code{pgcCombinePredxFiles} relies on the directory containing... 
+#' @importFrom DBI dbConnect dbSendQuery dbClearResult dbDisconnect
+#' @importFrom RSQLite RSQLite
+#' @import data.table
 #' @export
 
-pgcCombinePredxFiles <- function(){
+pgcCombinePredxFiles <- function(predDir) {
+  
   #creats list of all database filenames
-  fnames <- list.files(pattern = ".db$", full.names = TRUE, recursive = TRUE)
-  
+  fnames <- list.files(path = predDir, pattern = ".db$", full.names = TRUE)
   #creates empty list that we will add to
-  datlist <- list()
+  datList <- lapply(fnames, pgcGetPredxWeights)
+  dat <- rbindlist(datList)
+  dat[]
   
-  #loops through filenames
-  for (f in fnames) {
-    
-    tmp <- dbConnect(SQLite(), dbname = f)#connects to database
-    res <- dbSendQuery(tmp, "SELECT * FROM weights;")#grabs data from weights table
-    dat <- dbFetch(res,n = -1)#returns data and places into array dat
-    dbClearResult(res)#empties our return variable
-    dbDisconnect(tmp)#closes our db connection
-    dat$tissue <- basename(dirname(f))#creates a column for tissue by directory name
-    datlist[[basename(f)]] <- dat#adds iteration of data to our list
-    
-  }
-  dat <- do.call(rbind, datlist)#combines all list in datlist into one data frame
-  return(dat)
+}
+
+pgcGetPredxWeights <- function(predDB) {
+  
+  con <- dbConnect(SQLite(), dbname = predDB) # connects to database
+  # grabs data from weights table
+  res <- dbSendQuery(con, "SELECT * FROM weights;") 
+  dat <- as.data.table(dbFetch(res, n = -1))
+  dat[ , tissue := gsub("TW_|_0.5.db", "", basename(predDB))]
+  dbClearResult(res) # empties our return variable
+  dbDisconnect(con) # closes our db connection
+  dat[]
+  
 }
--- R/pgcDownloadPrediXcan.R
+++ R/pgcDownloadPrediXcan.R
@@ -3,8 +3,16 @@
 #' @param destDir character of length 1, the download directory
 #' @export
 
-pgcDownloadPrediXcan <- function(destDir = "PrediXcan") {
-  dir.exists(destDir) 
-  dir.create(destDir)
-  download.file("https://s3.amazonaws.com/predictdb2/deprecated/GTEx-V6p-HapMap-2016-09-08.tar.gz", destfile = destDir)
+pgcDownloadPrediXcan <- function(destDir = "PrediXcan", overwrite = FALSE) {
+  if (!dir.exists(destDir)) {
+    dir.create(destDir)
+  } else if (!overwrite) {
+    unlink(destDir, recursive = TRUE, force = TRUE)
+    dir.create(destDir)
+  } else {
+    stop("")
+  }
+  predUrl <- "https://s3.amazonaws.com/predictdb2"
+  u <- "deprecated/GTEx-V6p-HapMap-2016-09-08.tar.gz"
+  download.file(paste(predUrl, u, sep = "/"), destfile = destDir)
 }
