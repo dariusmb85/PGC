662451231a924e532e7834049176ce89473f4267
Darius Bost <dariusbost@rciadmins-Air.attlocal.net>
2021-05-08 15:57:11 -0400

    Update figure formatting
    removes rs and bp columns for paper as well as underscore in predictor name column.

--- R/Figure_building.R
+++ R/Figure_building.R
@@ -8,7 +8,9 @@ list_files <- list.files(pattern = "*Anno.txt")
 myDat <- fread(list_files)
 myDat <- myDat[order(Bonfphom)]
 myDat2 <- myDat[Bonfphom < 0.05]
-myDat$rs <- NULL
-print(xtable(myDat,digits = c(0,0,-2,-2,-2,-2,-2,-2,0,0)),
+myDat2$Tissue <- gsub("^_","",myDat2$Tissue)
+myDat2$rs <- NULL
+myDat2$bp <- NULL
+print(xtable(myDat2,digits = c(0,0,-2,-2,-2,-2,-2,-2,0,0)),
       floating=FALSE,latex.environments=NULL,
       include.rownames = FALSE, booktabs = TRUE)
