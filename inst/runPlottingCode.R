bc32ba8393ed6815be46581f877a71148a138273
Darius Bost <dariusbost@rciadmins-MacBook-Air.local>
2022-09-13 11:37:43 -0400

    Temp edits
    These will change again soon.

--- inst/runPlottingCode.R
+++ inst/runPlottingCode.R
@@ -1,10 +1,11 @@
 #make forest plots by running plottingCode.R
 
 source('~/GitHub/PGC/inst/plottingCode.R')
-fls <- list.files(pattern = "*datObj",recursive = TRUE)
+fls <- list.files(pattern = "*datObj",recursive = FALSE)
 l <- lapply(fls, readRDS)
 names(l) <- sub(".datObj", "", basename(fls))
-pdf("ForestPlot_Supplemental.pdf")
+pdf("AcrossTissue_FP_LOAD_Supplemental.pdf")
+l <- lapply(l, fixInf)
 lapply(l,pltCPA)
 dev.off()
 
